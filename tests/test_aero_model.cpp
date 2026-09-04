b3576f259510314ffb26549c76b6ce443fd9eb03
Fahd Ahmad <fahd.ahmad320@gmail.com>
2026-09-04 12:15:44 +0100

    GTest implementation for test_aero_model and test_integrator

--- tests/test_aero_model.cpp
+++ tests/test_aero_model.cpp
@@ -1,1 +1,17 @@
+TEST(AeroModelTest, ZeroAlphaProducesZeroLiftForZeroControl)
+{
+    sim::Vehicle vehicle{};
+    sim::State state{};
+    sim::ControlInput control{};
 
+    state.V = 100.0;
+    state.theta = 0.0;
+    state.gamma = 0.0;
+
+    sim::Aero_model model;
+
+    const auto forces =
+        model.evaluate(vehicle, state, control, 1.225);
+
+    EXPECT_NEAR(forces.lift, 0.0, 1e-9);
+}
--- tests/test_integrator.cpp
+++ tests/test_integrator.cpp
@@ -1,1 +1,33 @@
+#include <gtest/gtest.h>
 
+#include "Integrator.h"
+
+TEST(IntegratorTest, ConstantDerivativeProducesExpectedState)
+{
+    sim::State initial_state{};
+
+    initial_state.x = 0.0;
+
+    // Constant system: dx/dt = 10 m/s.
+    // All other state derivatives remain zero.
+    const auto constant_derivative =
+        [](double /*time*/, const sim::State& /*state*/)
+        {
+            sim::StateDerivative derivative{};
+            derivative.x_dot = 10.0;
+
+            return derivative;
+        };
+
+    const double t = 0.0;
+    const double dt = 0.1;
+
+    const auto next_state =
+        sim::rk4_step(
+            constant_derivative,
+            initial_state,
+            t,
+            dt);
+
+    EXPECT_NEAR(next_state.x, 1.0, 1e-9);
+}
